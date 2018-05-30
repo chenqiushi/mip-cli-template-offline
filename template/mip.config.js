57c5818fd4f1be155a0f2d9a87cd4b9a6c99fb14
chenqiushi <chenqiushi@baidu.com>
2018-05-30 21:29:53 +0800

    offline test

--- template/mip.config.js
+++ template/mip.config.js
@@ -3,19 +3,28 @@
  * @author
  */
 
-module.exports = {
 
+module.exports = {
+  dev: {
     /**
      * 启动mip server调试的端口号
      *
      * @type {number}
      */
-    port: 8000,
+    port: 8111,
 
     /**
      * 启用调试页面自动刷新
      *
      * @type {boolean}
      */
-    livereload: true
-};
+    livereload: true,
+
+    /**
+     * server 启动自动打开页面，false 为关闭
+     *
+     * @type {string|boolean}
+     */
+    autoopen: '/example/index.html'
+  }
+}
